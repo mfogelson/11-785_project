69f86ccf8f304825f23c5a391426af94843af547
Chris Dare <18426797+dexios1@users.noreply.github.com>
2020-12-01 09:27:16 +0200

    Revert "App update"

--- app.py
+++ app.py
@@ -1,49 +1,32 @@
 """The :mod:`streamlit_app` serves as a demo application to show the poetry generation API system at work.
 This runs on sreamlit
 """
-# Author: Christopher Dare, Xinkai Chen
+# Author: Christopher Dare
 import json
 
 import requests
 import streamlit as st
-import app_generate
-import app_turing
 
-base_url = "http://127.0.0.1:8080"
+base_url = "http://0.0.0.0:8080"
 generation_endpoint = "generate"
-retrieval_endpoint = "retrieve"
 
-PAGES = {
-    "Let's write a poem": app_generate,
-    "Guess who wrote it": app_turing
-}
-st.sidebar.title('Try me!')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+st.title("Youshen Poetry Generator")
+st.write("Poems you'll love by a friendly AI. ")  # description
 
-# we don't need this for now
-def app():
-    pass
+text_prompt = "My grandfather's clock was too large for the shelf"
+user_input = ""
+# user_input = st.text_input("Starter text", text_prompt)
 
-    # if st.button("Generate a poem"):
-    #     app_generate.generate()
-    #     # payload = dict()
-    #     # payload["user_input"] = str(user_input)
-    #     # payload = json.dumps(payload)
-    #     # response = requests.post(f"{base_url}/{generation_endpoint}", data=payload)
-    #     # data = response.json()
-    #     # output = "An error occured"
-    #     # if data["success"]:
-    #     #     output = data["payload"]
-    #     # st.write(output)
-    #
-    # if st.button("Retrieve a limerick"):
-    #     response = requests.post(f"{base_url}/{retrieval_endpoint}")
-    #     data = response.json()
-    #     output = "An error occured"
-    #     if data["success"]:
-    #         output = data["text"]
-    #     st.write(output)
+if st.button("Generate a poem"):
+    payload = dict()
+    payload["user_input"] = str(user_input)
 
-
+    payload = json.dumps(payload)
+    st.write("Posting")
+    st.write(payload)
+    response = requests.post(f"{base_url}/{generation_endpoint}", data=payload)
+    data = response.json()
+    output = "An error occured"
+    if data["success"]:
+        output = data["payload"]
+    st.title(output)
